45f93fa7e0f1d2492411f46d44bfb909885bdfb1
Garry Cacho <jonathan.atienza1986@protonmail.com>
2026-04-13 17:48:24 +0800

    giannah-mod-user.php-model87

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -58,12 +58,12 @@ class ChatController extends Controller
                     'temperature' => 0 // Set to 0 for consistent JSON structure
                 ]);
 
-            return Inertia::render('viewjs/chat/index', [
+            return Inertia::render('viewjs/chat/index_automation', [
                 //'response' => ["data" => $response->json()],
                 'response' => $response->json('choices.0.message'),
             ]);
 
         } else
-            return Inertia::render('viewjs/chat/index'); // Renders the Vue component
+            return Inertia::render('viewjs/chat/index_automation'); // Renders the Vue component
     }
 }
